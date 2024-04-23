f270c0b431189cf6abcdb7f25f1b25b62455f9ae
lazebird <lazebird@163.com>
2024-04-23 22:45:42 +0800

    feat: add more test log and other infos

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# @lazebird/vue3template
+# @lazebird/tauri
 
 ## 介绍
+- 本项目用于定位和记录一个tauri app在Windows上白屏的问题
+- 调试发现`src\store\locale.ts:44`导入中文语言存在问题，会卡死导致页面白屏
+- 调试版本可以通过`pnpm tauri build --debug`进行编译，运行时右键可以打开开发者工具并查看调试日志
 
-- vue3+vite 项目模板
-- <a href="https://lazebird.github.io/vue3template/" target="_blank">预览</a>
+# 截图
+## 故障
+![abnormal](./doc/abnormal.png)
+
+## 正常
+![normal](./doc/normal.png)
--- src/store/locale.ts
+++ src/store/locale.ts
@@ -1,47 +1,55 @@
-import { ref } from 'vue';
-import { defineStore } from 'pinia';
-import { store } from './index';
-import { invoke } from '@tauri-apps/api';
-import { useConfigStoreOut } from './config';
-import { LocaleConf } from '#/tauri';
-import { message as alert } from 'ant-design-vue';
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import { store } from "./index";
+import { invoke } from "@tauri-apps/api";
+import { useConfigStoreOut } from "./config";
+import { LocaleConf } from "#/tauri";
+// import { message as alert } from "ant-design-vue";
 
 function navlang2locale(navlang: string) {
   const map: any = {};
-  map['zh'] = 'zh_CN';
-  map['en'] = 'en_US';
-  return map[navlang] || navlang.replace('-', '_');
+  map["zh"] = "zh_CN";
+  map["en"] = "en_US";
+  return map[navlang] || navlang.replace("-", "_");
 }
 async function getLocaleAsync(localeList: LocaleConf[], fallback: string) {
   let navlocale = navlang2locale(navigator.language);
-  console.log('navlocale %s, localeList %s', navlocale, JSON.stringify(localeList.map((l) => l.value)));
+  console.log("navlocale %s, localeList %s", navlocale, JSON.stringify(localeList.map((l) => l.value)));
   if (!localeList?.find((l: any) => l.value === navlocale)) navlocale = fallback;
   return navlocale;
 }
 
-export const useLocaleStore = defineStore('locale', () => {
+export const useLocaleStore = defineStore("locale", () => {
   const configStore = useConfigStoreOut();
-  const fallback = 'en_US';
+  const fallback = "zh_CN";
   const localeList = ref([] as LocaleConf[]);
-  const conf_lang = ref('');
-  const cur_lang = ref('');
+  const conf_lang = ref("");
+  const cur_lang = ref("");
   const messages = ref({} as any);
   async function load(locale: string) {
     if (!messages.value[locale]) {
       let message: any = {};
+      let count = 0;
       try {
-        message = JSON.parse(await invoke('read_file', { dname: '', fname: `${locale}.json` }));
-      } catch (e) {
-        console.error('load customize %s.json error: %o', locale, e);
+        message = JSON.parse(await invoke("read_file", { dname: "", fname: `${locale}.json` }));
+      } catch (e: any) {
+        console.warn("load customize %s.json error: %s", locale, e.toLocaleString());
         message = undefined;
+        console.log("test ", ++count);
       }
       try {
+        console.log("test ", ++count);
         if (!message) message = await import(`../../langs/${locale}.json`);
-        const antdLocale = await import(`../../node_modules/ant-design-vue/es/locale/${locale}.js`);
+        console.log("test ", ++count);
+        const antdLocale: any = await import(`../../node_modules/ant-design-vue/es/locale/${locale}.js`);
+        // const antdLocale: any = undefined; // use this line instead of the line above, to fix blank screen issue in chinese env
+        console.log("test ", ++count);
         messages.value[locale] = { ...message, antdLocale: antdLocale?.default };
-        console.log('update locale %s, message %o', locale, messages.value[locale]);
-      } catch (e) {
-        alert.error(`load internal ${locale}.json error: ${e}`, 5);
+        console.log("update locale %s, message %o", locale, messages.value[locale]);
+      } catch (e: any) {
+        // alert.error(`load internal ${locale}.json error: ${e}`, 5);
+        console.log("test ", ++count);
+        console.error("load internal locale %s error: %s", locale, e.toLocaleString());
         messages.value[locale] = {};
       }
     }
@@ -54,7 +62,7 @@ export const useLocaleStore = defineStore('locale', () => {
     localeList.value = configStore.conf.localeList ?? [];
     conf_lang.value = configStore.conf.locale;
     cur_lang.value = conf_lang.value?.length ? conf_lang.value : await getLocaleAsync(localeList.value, fallback);
-    console.log('conf lang %s, cur lang %s', conf_lang.value, cur_lang.value);
+    console.log("conf lang %s, cur lang %s", conf_lang.value, cur_lang.value);
     await load(fallback);
     return { locale: cur_lang.value, message: await load(cur_lang.value) };
   }
